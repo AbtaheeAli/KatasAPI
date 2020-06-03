db16888427c9bee9da9b7f5c96fd0d2d546fba9d
Abtahee Ali <abtaheeali@gmail.com>
2020-06-03 18:23:46 -0400

    Added Mumbling and explosion controller

---
+++ Controllers/MumblingController.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KatasAPI.Controllers
+{
+    [ApiController]
+
+    [Route("[controllers]")]
+    public class MumblingController : ControllerBase
+    {
+
+    }
+}
