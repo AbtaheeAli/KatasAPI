2c9c79bf18c7ddd23c808c2d6f712e8329c9a8d0
Abtahee Ali <abtaheeali@gmail.com>
2020-06-03 19:30:55 -0400

    Mumbling controller is fully functional

--- Controllers/ExplosionController.cs
+++ Controllers/ExplosionController.cs
@@ -1,2 +1,30 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+
+namespace KatasAPI.Controllers
+{
+    [ApiController]
+
+    [Route("[controller]")]
+    public class ExplosionController : ControllerBase
+    {
+        [HttpGet("{explosions}")]
+
+        public string Explosions(string explosion)
+        {
+            var answer = "";
+
+            foreach (var character in explosion)
+            {
+                var number = int.Parse(character.ToString());
+
+                for (var n = 0; n < number; n++)
+                {
+                    answer = answer + character;
+                }
+            }
+            return answer;
+        }
+    }
+}
--- Controllers/MumblingController.cs
+++ Controllers/MumblingController.cs
@@ -1,13 +1,28 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KatasAPI.Controllers
 {
     [ApiController]
 
-    [Route("[controllers]")]
+    [Route("[controller]")]
     public class MumblingController : ControllerBase
     {
+        [HttpGet("{mumbling}")]
 
+        public string Mumble(string mumbling)
+        {
+            var answer = "";
+            var count = 0;
+            foreach (char character in mumbling.ToUpper())
+            {
+                if (count != 0) answer += "-";
+                answer += character;
+                answer += new String(char.ToLower(character), count);
+                ++count;
+            }
+            return answer;
+        }
     }
 }
